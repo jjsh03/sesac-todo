10fb80ac0b391ad17bf8c46789cd0d6ae23929ae
jjsh03 <aberrant03@gmail.com>
2023-10-11 18:44:32 +0900

    feat: todo 추가하기

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import Todo from './components/Todo';
+import AddTodo from './components/AddTodo';
 
 function App() {
   const [todoItems, setTodoItems] = useState([
@@ -24,8 +25,21 @@ function App() {
       done: false,
     },
   ]);
+
+  // todoItems 상태에 새로운 투두를 추가하는 일
+  const addItem = (newItem) => {
+    console.log(newItem); // { title: '저녁먹기' }
+
+    // newItem id 키 값 넣고, newItem done 키 값
+    newItem.id = todoItems.length + 1;
+    newItem.done = false;
+
+    // todoItems 배열에 newItem을 추가
+    setTodoItems([...todoItems, newItem]);
+  };
   return (
     <div className="App">
+      <AddTodo addItem={addItem} />
       {/* todoItems 반복, props 데이터(투두 객체)를 자식 컴포넌트에게 전달 */}
       {todoItems.map((item) => (
         <Todo key={item.id} item={item} />
