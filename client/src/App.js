42de5b48d233bd54857cf33718e64d7708a07e13
jjsh03 <aberrant03@gmail.com>
2023-10-16 02:25:54 +0900

    feat: doing/done 실시간 업데이트

--- client/src/App.js
+++ client/src/App.js
@@ -4,8 +4,9 @@ import AddTodo from './components/AddTodo';
 import axios from 'axios';
 
 function App() {
-  // console.log(process.env.REACT_APP_DB_HOST);
   const [todoItems, setTodoItems] = useState([]);
+  const [doingItems, setDoingItems] = useState(0);
+  const [doneItems, setDoneItems] = useState(0);
 
   useEffect(() => {
     const getTodos = async () => {
@@ -16,8 +17,16 @@ function App() {
     getTodos();
   }, []);
 
+  useEffect(() => {
+    const doingCount = todoItems.filter(item => !item.done).length;
+    const doneCount = todoItems.filter(item => item.done).length;
+
+    setDoingItems(doingCount);
+    setDoneItems(doneCount);
+  }, [todoItems]);
+
   //todoItems 상태에 새로운 투두를 추가하는 일
-  const addItem = async (newItem) => {
+  const addItem = async newItem => {
     // console.log(newItem); //{title: '저녁 먹기}
 
     // //newItems id 키 값 넣고, neItem done 키 값
@@ -29,46 +38,52 @@ function App() {
 
     const res = await axios.post(
       `${process.env.REACT_APP_DB_HOST}/todo`,
-      newItem
+      newItem,
     );
     // console.log(res);
     setTodoItems([...todoItems, res.data]);
   };
 
   // todoItems 상태에 특정 투두를 삭제하는 일
-  const deleteItem = async (targetItem) => {
+  const deleteItem = async targetItem => {
     // console.log(targetItem);
 
     await axios.delete(
-      `${process.env.REACT_APP_DB_HOST}/todo/${targetItem.id}`
+      `${process.env.REACT_APP_DB_HOST}/todo/${targetItem.id}`,
     );
 
-    const newTodoItems = todoItems.filter((item) => item.id !== targetItem.id);
+    const newTodoItems = todoItems.filter(item => item.id !== targetItem.id);
     setTodoItems(newTodoItems);
   };
 
-  const updateItem = async (targetItem) => {
+  const updateItem = async targetItem => {
     await axios.patch(
       `${process.env.REACT_APP_DB_HOST}/todo/${targetItem.id}`,
-      targetItem
+      targetItem,
+    );
+
+    setTodoItems(prevItems =>
+      prevItems.map(item => (item.id === targetItem.id ? targetItem : item)),
     );
   };
 
   return (
-    <div className="App">
-      <div>Total: {todoItems.length}</div>
+    <div className='App'>
       <AddTodo addItem={addItem} />
+      <div>
+        <p>Total: {todoItems.length}</p>
+        <p>Doing: {doingItems}</p>
+        <p>Done: {doneItems}</p>
+      </div>
       {/* todoItems 반복,  props 데이터(투두 객체)를 자식 컴포넌트에게 전달 */}
-      {todoItems
-        .sort((a, b) => b.id - a.id)
-        .map((item) => (
-          <Todo
-            key={item.id}
-            item={item}
-            deleteItem={deleteItem}
-            updateItem={updateItem}
-          />
-        ))}
+      {todoItems.map(item => (
+        <Todo
+          key={item.id}
+          item={item}
+          deleteItem={deleteItem}
+          updateItem={updateItem}
+        />
+      ))}
     </div>
   );
 }
--- client/src/components/Todo.js
+++ client/src/components/Todo.js
@@ -1,52 +1,54 @@
 import React, { useState } from 'react';
 
+// checkbox와 label을 렌더링 하는 투두 하나!
 export default function Todo({ item, deleteItem, updateItem }) {
+  //   console.log(item);
   const [todoItem, setTodoItem] = useState(item);
   const { id, title, done } = todoItem;
   const [readOnly, setReadOnly] = useState(true);
 
   const onDeleteButtonClick = () => {
-    deleteItem(todoItem); //todoItem은 객체, id 값만 받아와도 되지만 추후 수정의 편의성 위해 값 다 받음
+    deleteItem(todoItem);
   };
 
-  // title 클릭하면 readOnly를 false변경 (수정 가능하도록)!!
+  // title 클릭하면 readOnly를 false 변경 (수정 가능하도록!!)
   const offReadOnlyMode = () => {
     setReadOnly(false);
   };
 
   // title 수정
-  const editEventHandler = (e) => {
+  const editEventHandler = e => {
     const { title, ...rest } = todoItem;
     setTodoItem({
-      title: e.target.value, //현재 유저가 입력하고 있는  value값
+      title: e.target.value,
       ...rest,
     });
   };
 
-  // Enter 키 누르면 readOnly true로 변경
-  const editKeyEventHandler = (e) => {
+  // Enter 키 누르면 readOnly true 변경
+  const editKeyEventHandler = e => {
     if (e.key === 'Enter') {
       setReadOnly(true);
-      updateItem(todoItem);
+      updateItem(todoItem); // 엔터키 누르면 저장
     }
   };
 
-  //checkbox 상태 업데이트
-  const checkboxEventHandler = (e) => {
+  // checkbox 상태 업데이트
+  const checkboxEventHandler = e => {
     // console.log(e);
     const { done, ...rest } = todoItem;
-    const updateItem = {
+    const updatedItem = {
       done: e.target.checked,
       ...rest,
     };
-    setTodoItem(updateItem);
-    updateItem(updateItem);
+    setTodoItem(updatedItem);
+    updateItem(updatedItem); // 체크박스 변경시 저장
   };
 
   return (
     <div>
       <input
-        type="checkbox"
+        type='checkbox'
         name={`todo${id}`}
         id={`todo${id}`}
         defaultChecked={done}
@@ -54,16 +56,14 @@ export default function Todo({ item, deleteItem, updateItem }) {
       />
       {/* <label htmlFor={`todo${id}`}>{title}</label> */}
       <input
-        type="text"
+        type='text'
         value={todoItem.title}
         readOnly={readOnly}
         onClick={offReadOnlyMode}
         onChange={editEventHandler}
         onKeyDown={editKeyEventHandler}
       />
-      {/* 클릭시 readOnly를 통해 boolean변화로 수정이 가능한 상태로 변환 */}
-      {/* onChange;로 변화하는 수정 상태를 반영 */}
-      <button onClick={onDeleteButtonClick}>delete</button>
+      <button onClick={onDeleteButtonClick}>DELETE</button>
     </div>
   );
 }
